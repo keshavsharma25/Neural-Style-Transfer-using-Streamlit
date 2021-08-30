c793a299388b66909d053e67d129a1f67d5f5fd7
Keshav Sharma <keshavsharma2250800@gmail.com>
2021-08-30 14:44:42 +0530

    removed footer note

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from PIL import Image
 import random
 import style
 
-st.title("Pytorch Image Style Transfer")
+st.title("Pytorch Image Style Transfer")  # Title
 
-img_file = st.sidebar.file_uploader('Upload Image', type=['jpg', 'png'])
+img_file = st.sidebar.file_uploader('Upload Image', type=['jpg', 'png']) 
 
 style_name = st.sidebar.selectbox('Select Style',
                                   ('candy', 'mosaic', 'rain_princess', 'udnie', 'alan-scales'))
 
 model = 'saved_models/'+style_name+'.pth'
+
 if img_file is not None:
     input_image = Image.open(img_file)
     output_image = 'images/output-images/' + style_name + str(random.randint(0, 100)) + '.jpg'
@@ -28,3 +29,12 @@ if img_file is not None:
         out_image = Image.open(output_image)
         st.image(out_image, width=400)
 
+
+# To hide footer
+hide_streamlit_style = """
+            <style>
+            footer {visibility: hidden;}
+            </style>
+            """
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
